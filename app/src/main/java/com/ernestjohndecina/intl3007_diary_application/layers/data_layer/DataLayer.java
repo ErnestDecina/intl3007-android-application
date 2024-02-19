5f5addffe30839c52642254cc4b57ba922000a10
ErnestJohnDecina <ernestjohn007@gmail.com>
2024-02-19 20:51:30 +0000

    Add DataLayer.java prototype functions

--- app/src/main/java/com/ernestjohndecina/intl3007_diary_application/layers/data_layer/DataLayer.java
+++ app/src/main/java/com/ernestjohndecina/intl3007_diary_application/layers/data_layer/DataLayer.java
@@ -2,4 +2,41 @@ package com.ernestjohndecina.intl3007_diary_application.layers.data_layer;
 
 public class DataLayer {
 
+    /**
+     * DataLayer Constructor
+     */
+    DataLayer() {
+
+    }
+
+    /**
+     * Stores Encrypted Images into the storage
+     * @
+     */
+    public void storeImage() {
+
+    }
+
+    /**
+     * Reads Encrypted Images from storage
+     */
+    public void readImage() {
+
+    }
+
+    /**
+     * Stores Encrypted Audio into the storage
+     */
+    public void storeAudio() {
+
+    }
+
+    /**
+     * Reads Encrypted Audio from storage
+     */
+    public void readAudio() {
+
+    }
+
+
 }
