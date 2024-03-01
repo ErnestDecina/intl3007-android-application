cb75365deff3c62702ac6228f6a92d235fec2a97
ErnestJohnDecina <ernestjohn007@gmail.com>
2024-03-01 22:53:52 +0000

    Add Activity Classes

---
+++ app/src/main/java/com/ernestjohndecina/intl3007_diary_application/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.ernestjohndecina.intl3007_diary_application.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ernestjohndecina.intl3007_diary_application.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
