75e83af37093a54e7403746a72c9bdac3aabd21b
ErnestJohnDecina <ernestjohn007@gmail.com>
2024-03-10 07:51:20 +0000

    Added SystemFeatures and ExecutorService to LoginActivity

--- app/src/main/java/com/ernestjohndecina/intl3007_diary_application/activities/LoginActivity.java
+++ app/src/main/java/com/ernestjohndecina/intl3007_diary_application/activities/LoginActivity.java
@@ -5,12 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ernestjohndecina.intl3007_diary_application.R;
+import com.ernestjohndecina.intl3007_diary_application.layers.system_features.SystemFeatures;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
+    ExecutorService executorService;
+    SystemFeatures systemFeatures;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        createDependencies();
+    }
+
+
+    private void createDependencies() {
+        createThreadExecutor();
+        createSystemFeatures();
+    }
+
+    private void createThreadExecutor() {
+        executorService = new ThreadPoolExecutor(
+                4,
+                10,
+                5L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque<>()
+        );
+    }
+
+    private void createSystemFeatures() {
+        this.systemFeatures = new SystemFeatures(
+                this,
+                this.executorService
+        );
+
     }
 }
