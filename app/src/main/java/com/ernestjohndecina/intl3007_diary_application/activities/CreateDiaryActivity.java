52824463392f7c4cc82884c59e091560dd757704
ErnestJohnDecina <ernestjohn007@gmail.com>
2024-03-16 01:57:29 +0000

    Added Dependencies to CreateDiaryActivity

--- app/src/main/java/com/ernestjohndecina/intl3007_diary_application/activities/CreateDiaryActivity.java
+++ app/src/main/java/com/ernestjohndecina/intl3007_diary_application/activities/CreateDiaryActivity.java
@@ -5,12 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ernestjohndecina.intl3007_diary_application.R;
+import com.ernestjohndecina.intl3007_diary_application.layers.system_features.SystemFeatures;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class CreateDiaryActivity extends AppCompatActivity {
+    ExecutorService executorService;
+    SystemFeatures systemFeatures;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_diary);
+
+        createDependencies();
+    }
+
+    private void createDependencies() {
+        createThreadExecutor();
+        createSystemFeatures();
+    }
+
+    private void createThreadExecutor() {
+        executorService = new ThreadPoolExecutor(
+                4,
+                10,
+                5L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque<>()
+        );
+    }
+
+    private void createSystemFeatures() {
+        this.systemFeatures = new SystemFeatures(
+                this,
+                this.executorService
+        );
     }
 }
