f99b874d55594de487d8584e7fe98cc49331930e
C21379843 Cecilia Luan <c21379843@mytudublin.ie>
2024-03-24 08:25:36 +0900

    created adapter item

--- app/src/main/java/com/ernestjohndecina/intl3007_diary_application/fragments/HomeDiaryFragment.java
+++ app/src/main/java/com/ernestjohndecina/intl3007_diary_application/fragments/HomeDiaryFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.ernestjohndecina.intl3007_diary_application.R;
+import com.ernestjohndecina.intl3007_diary_application.database.entities.User;
 import com.ernestjohndecina.intl3007_diary_application.layers.system_features.SystemFeatures;
 
 public class HomeDiaryFragment extends Fragment {
@@ -22,17 +23,29 @@ public class HomeDiaryFragment extends Fragment {
         return new HomeDiaryFragment();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home_diary, container, false);
-
         // Set up greeting text with the username
-        TextView greetingTextView = view.findViewById(R.id.greetingTextView);
-        String username = getArguments() != null ? getArguments().getString("username", "User") : "User";
-        greetingTextView.setText("Good Morning " + username);
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_home_diary, container, false);
 
-        return view;
-    }
+            // Set up greeting text with the username
+            TextView greetingTextView = view.findViewById(R.id.greetingTextView);
+
+            // Check if systemFeatures is not null and userFeatures is available
+            if (systemFeatures != null && systemFeatures.userFeatures != null) {
+                // Get the user information
+                User userInfo = systemFeatures.userFeatures.getUserAccountDetails();
+                String firstName = ((User) userInfo).firstName;
+
+                // Display the greeting with the user's first name
+                greetingTextView.setText("Good Morning, " + firstName);
+            } else {
+                // Fallback to a default greeting if user information is not available
+                greetingTextView.setText("Good Morning, User");
+            }
+
+            return view;
+        }
 
     public void setSystemFeatures(SystemFeatures systemFeatures) {
         this.systemFeatures = systemFeatures;
